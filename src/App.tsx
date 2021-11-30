d39a11a65293cd06ece42b1ffa019fc5ca223cf8
patricksousamacedo <patricksouzamacedo@gmail.com>
2021-11-30 10:16:28 -0300

    commit de hj

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import * as C from './App.styles';
+import * as Photos from './services/photos';
+import { Photo } from './types/Photo';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false); 
+  const [photos, setPhotos] = useState<Photo[]>([]); 
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll()); 
+      setLoading(false); 
+    }
+    getPhotos();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {loading && // agora vamos adicinoar aqui abaixo mais opções para a imagem de loading
+          <C.ScreenWarning>
+            <div className="emoji">🤚</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
+        {!loading && photos.length > 0 &&
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        }
+        {!loading && photos.length === 0 && // quando não tiver carregando e não tiver fotos
+          <C.ScreenWarning>
+            <div className="emoji">😫</div>
+            <div>Não há fotos cadastradas</div>
+          </C.ScreenWarning>
+        }
+      </C.Area>
+    </C.Container>
+  )
 }
+
 
 export default App;
